e3ef5b09aabdb38ce715ddee38118e0444940d2d
phiest34 <nur-14@list.ru>
2020-05-30 21:35:56 +0500

    downloading script

--- Bank/views.py
+++ Bank/views.py
@@ -4,7 +4,18 @@ from Bank.models import bank_b
 from Bank.models import bank_d
 from Bank.models import banks
 from Bank.models import bank_n
+import datetime
+import rarfile
+import wget
+import os
 from dbfread import DBF
+
+
+def formatting(month):
+    if int(month) >= 10:
+        return str(month)
+    else:
+        return '0' + str(month)
 
 
 def index(request):
@@ -26,44 +37,60 @@ def choice(request):
 
 
 def load(request):
+    report = datetime.datetime(year=2020, month=1, day=1)
+    current_date = datetime.datetime.now()
+    directory = 'Bank/archives/'
+    extract = 'Bank/dbf_files/'
+    extract_list = os.listdir(extract)
+    files = os.listdir(directory)
+    while report.month < current_date.month:
+        report = report.replace(month=report.month + 1)
+        file = '123-' + str(report.year) + formatting(report.month) + '01.rar'
+        try:
+            filename = wget.download('http://old.cbr.ru/vfs/credit/forms/' + file)
+            os.rename(filename, u'' + os.getcwd() + '/Bank/archives/' + filename)
+            if file not in extract_list:
+                rf = rarfile.RarFile(directory + file)
+                rf.PATH_SEP = '/'
+                path = os.path.join(extract, file)
+                os.mkdir(path)
+                rf.extractall(path=extract + file)
+        except:
+            pass
     tableS = DBF('Bank/dbf_files/112019_123S.DBF', load=True, encoding="cp866")
     tableB = DBF('Bank/dbf_files/112019_123B.DBF', load=True, encoding="cp866")
     tableD = DBF('Bank/dbf_files/112019_123D.DBF', load=True, encoding="cp866")
     tableN = DBF('Bank/dbf_files/112019_123N.DBF', load=True, encoding="cp866")
-    list_s = list()
-    list_b = list()
-    list_d = list()
-    list_n = list()
     if bank_s.objects.count() != len(tableN):
         for record in range(len(tableS)):
-            list_s.append(bank_s.objects.get_or_create(REGN=tableS.records[record]['REGN'],
-                                                       C1_S=tableS.records[record]['C1_S'],
-                                                       C31_S=tableS.records[record]['C31_S'],
-                                                       C2_S=tableS.records[record]['C2_S'],
-                                                       C32_S=tableS.records[record]['C32_S']))
+            bank_s.objects.get_or_create(REGN=tableS.records[record]['REGN'],
+                                         C1_S=tableS.records[record]['C1_S'],
+                                         C31_S=tableS.records[record]['C31_S'],
+                                         C2_S=tableS.records[record]['C2_S'],
+                                         C32_S=tableS.records[record]['C32_S'])
 
     if bank_b.objects.filter().count() != len(tableB):
         for record in range(len(tableB)):
-            list_b.append(bank_b.objects.get_or_create(REGN=tableB.records[record]['REGN'],
-                                                       OKATO=tableB.records[record]['OKATO'],
-                                                       OKPO=tableB.records[record]['OKPO'],
-                                                       OGRN=tableB.records[record]['OGRN'],
-                                                       REGN_S=tableB.records[record]['REGN_S'],
-                                                       BIC=tableB.records[record]['BIC'],
-                                                       DT=tableB.records[record]['DT'],
-                                                       NAME_B=tableB.records[record]['NAME_B'],
-                                                       ADR=tableB.records[record]['ADR']))
+            bank_b.objects.get_or_create(REGN=tableB.records[record]['REGN'],
+                                         OKATO=tableB.records[record]['OKATO'],
+                                         OKPO=tableB.records[record]['OKPO'],
+                                         OGRN=tableB.records[record]['OGRN'],
+                                         REGN_S=tableB.records[record]['REGN_S'],
+                                         BIC=tableB.records[record]['BIC'],
+                                         DT=tableB.records[record]['DT'],
+                                         NAME_B=tableB.records[record]['NAME_B'],
+                                         ADR=tableB.records[record]['ADR'])
 
     if bank_d.objects.filter().count() != len(tableD):
         for record in range(len(tableD)):
-            list_d.append(bank_d.objects.get_or_create(
+            bank_d.objects.get_or_create(
                 REGN_C1=str(tableD.records[record]['REGN']) + str(tableD.records[record]['C1']),
-                C3=tableD.records[record]['C3']))
+                C3=tableD.records[record]['C3'])
 
     if bank_n.objects.filter().count() != len(tableN):
         for record in range(len(tableN)):
-            list_n.append(bank_n.objects.get_or_create(C1=tableN.records[record]['C1'],
-                                                       C2_1=tableN.records[record]['C2_1'],
-                                                       C2_2=tableN.records[record]['C2_2'],
-                                                       C2_3=tableN.records[record]['C2_3']))
+            bank_n.objects.get_or_create(C1=tableN.records[record]['C1'],
+                                         C2_1=tableN.records[record]['C2_1'],
+                                         C2_2=tableN.records[record]['C2_2'],
+                                         C2_3=tableN.records[record]['C2_3'])
     return render(request, 'data/main.html')
